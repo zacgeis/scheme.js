98279fb39688eaf095aaf3c7a6a311baf7f6fb39
zacgeis <zacgeis@gmail.com>
2015-06-12 15:25:18 -0500

    Add REPL like behavior.

--- spec/EvaluatorSpec.js
+++ spec/EvaluatorSpec.js
@@ -1,57 +1,66 @@
 describe("Evaluator", function() {
   it("should evaluate simple expressions", function() {
-    var input = '(+ 1 1)';
-    var result = Evaluator.main(input);
+    var str = '(+ 1 1)';
+    var result = Evaluator.input(str);
     expect(result).toEqual(2);
   });
   it("should evaluate nested expressions", function() {
-    var input = '(+ (+ 2 3) (+ 1 4))';
-    var result = Evaluator.main(input);
+    var str = '(+ (+ 2 3) (+ 1 4))';
+    var result = Evaluator.input(str);
     expect(result).toEqual(10);
   });
   it("should evaluate variable expressions", function() {
-    var input = '(define a 2) (+ (+ a 3) (+ 1 4))';
-    var result = Evaluator.main(input);
+    var str = '(define a 2) (+ (+ a 3) (+ 1 4))';
+    var result = Evaluator.input(str);
     expect(result).toEqual(10);
   });
   it("should evaluate defined expressions", function() {
-    var input = '(define (add a b) (+ a b)) (add 3 2)';
-    var result = Evaluator.main(input);
+    var str = '(define (add a b) (+ a b)) (add 3 2)';
+    var result = Evaluator.input(str);
     expect(result).toEqual(5);
   });
   it("should allow for mutable expressions", function() {
-    var input = '(define a 1) (set! a 3) (+ a 2)';
-    var result = Evaluator.main(input);
+    var str = '(define a 1) (set! a 3) (+ a 2)';
+    var result = Evaluator.input(str);
     expect(result).toEqual(5);
   });
   it("should allow for truthy expressions", function() {
-    var input = '(> 1 0)';
-    var result = Evaluator.main(input);
+    var str = '(> 1 0)';
+    var result = Evaluator.input(str);
     expect(result).toEqual(true);
   });
   it("should allow for truthy expressions", function() {
-    var input = '(< 1 0)';
-    var result = Evaluator.main(input);
+    var str = '(< 1 0)';
+    var result = Evaluator.input(str);
     expect(result).toEqual(false);
   });
   it("should allow for truthy expressions", function() {
-    var input = '(= 1 1)';
-    var result = Evaluator.main(input);
+    var str = '(= 1 1)';
+    var result = Evaluator.input(str);
     expect(result).toEqual(true);
   });
   it("should allow for conditional expressions", function() {
-    var input = '(if (= 1 1) 2 3)';
-    var result = Evaluator.main(input);
+    var str = '(if (= 1 1) 2 3)';
+    var result = Evaluator.input(str);
     expect(result).toEqual(2);
   });
   it("should handle dot expressions", function() {
-    var input = '(define (test . args) args) (test 1 2 3)'
-    var result = Evaluator.main(input);
+    var str = '(define (test . args) args) (test 1 2 3)'
+    var result = Evaluator.input(str);
     expect(result).toEqual([1, 2, 3]);
   });
   it("should allow for recursive expressions", function() {
-    var input = '(define (factorial n) (if (= n 0) 1 (* n (factorial (- n 1))))) (factorial 5)';
-    var result = Evaluator.main(input);
+    var str = '(define (factorial n) (if (= n 0) 1 (* n (factorial (- n 1))))) (factorial 5)';
+    var result = Evaluator.input(str);
     expect(result).toEqual(120);
+  });
+  it("should allow for repl like behavior", function() {
+    var str1 = '(define a 1)';
+    var str2 = '(set! a 3)'
+    var str3 = '(+ a 2)';
+    Evaluator.input(str1);
+    Evaluator.input(str2);
+    var result = Evaluator.input(str3);
+    expect(result).toEqual(5);
   });
 });
